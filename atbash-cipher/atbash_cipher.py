5b8048e6f80a251b52587dc0adfef19acaece837
John R Hageman <john.r.hageman@gmail.com>
2018-03-22 19:37:26 +0000

    more work on atbash

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -5,6 +5,9 @@ def encode(txt):
     return ' '.join([''.join(seq[i:i+5]) for i  in range(0, len(seq), 5)])
 
 def decode(txt):
-    pass
+    orig = 'abcdefghijklmnopqrstuvwxyz'
+    coded = orig[::-1]
+    return ''.join([orig[coded.find(x)] if x.isalpha() else x for i,x in enumerate(txt.lower().replace(' ','')) if x.isalnum()])
 
-print(encode("The quick brown fox jumps over the lazy dog."))
+print(encode("The testing  quick brown fox jumps over the lazy dog."))
+print(decode(encode("the quick brown fox")))
