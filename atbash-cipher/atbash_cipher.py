7aa483bc8975b02b9968f030acb6762e30dd29ba
John R Hageman <john.r.hageman@gmail.com>
2018-04-04 02:12:08 +0000

    updating, started on say

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,13 +1,15 @@
 def encode(txt):
     orig = 'abcdefghijklmnopqrstuvwxyz'
     coded = orig[::-1]
-    seq = [coded[orig.find(x)] if x.isalpha() else x for i,x in enumerate(txt.lower()) if x.isalnum()]
+    seq = [coded[orig.find(x)] if x.isalpha() else x for i,x in \
+           enumerate(txt.lower()) if x.isalnum()]
     return ' '.join([''.join(seq[i:i+5]) for i  in range(0, len(seq), 5)])
 
 def decode(txt):
     orig = 'abcdefghijklmnopqrstuvwxyz'
     coded = orig[::-1]
-    return ''.join([orig[coded.find(x)] if x.isalpha() else x for i,x in enumerate(txt.lower().replace(' ','')) if x.isalnum()])
+    return ''.join([orig[coded.find(x)] if x.isalpha() else x for i,x in \
+                    enumerate(txt.lower().replace(' ','')) if x.isalnum()])
 
 print(encode("The testing  quick brown fox jumps over the lazy dog."))
 print(decode(encode("the quick brown fox")))
---
+++ say/say.py
@@ -0,0 +1,4 @@
+nummap = {0:"zero", 1:"one",2:"two"}
+
+print(nummap[1])
+
